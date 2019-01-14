e226f80165e108deeb9b2e8d6002d29d4fa21909
Sean 'Blitz' Homer <homer@therefore.ca>
2019-01-14 09:02:57 -0500

    Refreshing Site

---
+++ precache-manifest.5431fc77b7748aea2e071bcab2d28a03.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5a63c099747d5e3167dd87d1bef90ce",
+    "url": "/vue-card-api-app/fonts/materialdesignicons-webfont.a5a63c09.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-card-api-app/robots.txt"
+  },
+  {
+    "revision": "14eb505e0ed052177112",
+    "url": "/vue-card-api-app/js/chunk-vendors.daeaba59.js"
+  },
+  {
+    "revision": "77f59dab3c57bafc84bf",
+    "url": "/vue-card-api-app/js/app.b4956310.js"
+  },
+  {
+    "revision": "93f128ed879c56ce02c1c66779e6ff1e",
+    "url": "/vue-card-api-app/index.html"
+  },
+  {
+    "revision": "b65a7ff4c913b0ae8b715a3629fa463f",
+    "url": "/vue-card-api-app/img/materialdesignicons-webfont.b65a7ff4.svg"
+  },
+  {
+    "revision": "d1bdfb3838e2f78edf1ede85f56eabc9",
+    "url": "/vue-card-api-app/fonts/materialdesignicons-webfont.d1bdfb38.woff2"
+  },
+  {
+    "revision": "af695aec1806605828e23a5afcf06f55",
+    "url": "/vue-card-api-app/fonts/materialdesignicons-webfont.af695aec.ttf"
+  },
+  {
+    "revision": "187554eb3597e6ac3102b388b4038662",
+    "url": "/vue-card-api-app/fonts/materialdesignicons-webfont.187554eb.eot"
+  },
+  {
+    "revision": "14eb505e0ed052177112",
+    "url": "/vue-card-api-app/css/chunk-vendors.662d93ac.css"
+  },
+  {
+    "revision": "77f59dab3c57bafc84bf",
+    "url": "/vue-card-api-app/css/app.6edf660d.css"
+  }
+];
